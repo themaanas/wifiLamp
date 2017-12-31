aa8a3e47daf3da099ecb66941bc7f85c1ce2c663
Maanas Manoj <themaanas@gmail.com>
2017-12-30 20:30:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 var express = require('express');
 var gpio = require("rpio");
+
 var app = express();
+
+//Using pin 7
 var gpioPin = 7;
+
+//Initializes the lamp state to OFF
 gpio.open(gpioPin, gpio.OUTPUT, gpio.LOW);
+
+//Returns the status of the light
 app.get('/status/', function(req, res){
         res.send((gpio.read(gpioPin) ? 'high' : 'low'));
         console.log('status');
 });
+
+//Sets the light state to ON
 app.get('/on/', function(req, res){
         gpio.write(gpioPin, gpio.HIGH);
         console.log('on');
         res.send('1');
 });
+
+//Sets the light state to OFF
 app.get('/off/', function(req, res){
         gpio.write(gpioPin, gpio.LOW);
         console.log('off');
         res.send('0');
 });
+
+//Toggles the light
 app.get('/toggle/', function(req, res){
         if (gpio.read(gpioPin, gpio.INPUT) == 1){
                 gpio.write(gpioPin, gpio.LOW);
