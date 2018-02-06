d50d205e1adce64bc0f8ee594f840758aeade984
Maanas Manoj <themaanas@gmail.com>
2018-02-06 00:27:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var gpio = require('rpio');
 var app = express();
-var gpioPin = 7; //pin that the lamp is connected to
+var gpioPin = 7; //pin that the relay is connected to
 gpio.open(gpioPin, gpio.OUTPUT, gpio.LOW);
-app.get('/status/', function(req, res){ //returns status of lamp
+app.get('/status/', function(req, res){ //returns status of relay
         res.send((gpio.read(gpioPin) ? 'on':'off'));
         console.log('status');
 });
-app.get('/on/', function(req, res){ //turns lamp on
+app.get('/on/', function(req, res){ //turns relay on
         gpio.write(gpioPin, gpio.HIGH);
         console.log('on');
         res.send('1');
 });
-app.get('/off/', function(req, res){ //turns lamp off
+app.get('/off/', function(req, res){ //turns relay off
         gpio.write(gpioPin, gpio.LOW);
         console.log('off');
         res.send('0');
